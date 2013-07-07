507c29d3b63d31b6b810a85bec6c2ee0cb945646
Ben Ashman <ben.ashman@me.com>
2013-07-07 19:35:44 +0100

    pls ignore

--- app.rb
+++ app.rb
@@ -1,10 +1,7 @@
-#configure twitter
-Twitter.configure do |config|
-  config.consumer_key = 'rw5RHxWAeO7O5bZlA46w'
-  config.consumer_secret = 'kIO62Vw0C6HRhTJqqSYMOlDfa2Ui85qz0KuiZu9wA68'
-  config.oauth_token = '24029639-C9gwbdpuj2Elx7LdAyPPAkkws1dZUjWoQwGNjma7P'
-  config.oauth_token_secret = 'MBEDEUSCD0U0LOOAg91zmfED7whqkDq10PfO27js'
-end
+Bundler.require
+
+# require initializers
+Dir.glob("initializers/*.rb").each { |r| require_relative r }
 
 # require controllers
 Dir.glob("controllers/*.rb").each { |r| require_relative r }
--- helpers/helpers.rb
+++ helpers/helpers.rb
@@ -4,10 +4,12 @@ helpers do
     username.gsub(/^@/, "")
   end
 
+  # return sensibly-sized avatar
   def formatted_avatar(username)
     Twitter.user(username).profile_image_url.gsub("_normal", "")
   end
 
+  # return site root
   def base_url
     @base_url ||= "#{request.env['rack.url_scheme']}://#{request.env['HTTP_HOST']}"
   end
